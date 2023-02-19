50aabc5c29c721b9e5c7dd6e0b7ee5a5a049d786
muhammad-salar-syed <79195670+muhammad-salar-syed@users.noreply.github.com>
2023-02-19 23:44:50 +0100

    Update Unet_VGG16_model.py

--- Unet_VGG16_model.py
+++ Unet_VGG16_model.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D,concatenate,Activation,Conv2DTranspose,BatchNormalization,Dropout,Lambda
+from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D,Concatenate,Activation,Conv2DTranspose,BatchNormalization,Dropout,Lambda
 from tensorflow.keras.optimizers import Adam
 from keras.layers import Activation,MaxPool2D,Concatenate
 from tensorflow.keras.applications import VGG16
